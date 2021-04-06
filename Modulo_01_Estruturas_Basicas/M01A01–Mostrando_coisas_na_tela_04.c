599ce4af910fa1b2aaf0986be9837d6529bcee48
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-06 18:38:09 +0100

    M01A01 – Mostrando coisas na tela

---
+++ Modulo_01_Estruturas_Basicas/M01A01–Mostrando_coisas_na_tela_04.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main(){
+    printf("C é \n \"SUPER\" \nFácil!");
+}
+/*
+\n nova linha
+\t tabulação
+\b backspace
+\r retorno
+\\ barra
+\" aspas
+\? interrogação
+\a beep
+%% percentagem
+*/
