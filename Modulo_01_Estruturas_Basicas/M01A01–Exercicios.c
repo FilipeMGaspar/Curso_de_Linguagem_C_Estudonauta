b1ca2d13d5a5418582493aea4eb694202a0c22b7
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-06 18:45:32 +0100

    M01A01 – Mostrando coisas na tela

---
+++ Modulo_01_Estruturas_Basicas/M01A01–Exercicios.c
@@ -0,0 +1,12 @@
+/*
+<<<< EX001 - Listagem >>>>
+
+Listagem de Alunos
+Nome            Nota
+-----------------------
+Ana Beatriz     8.5
+Bianca Martins  9.0
+Cláudio Sá      5.5
+Giovana Silva   7.5
+*/
+
