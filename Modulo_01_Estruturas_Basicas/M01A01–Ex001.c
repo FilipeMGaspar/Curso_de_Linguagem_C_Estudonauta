5be56aa7a2adb4b6b2549bae6565dd08eea746a1
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-07 18:39:44 +0100

    M01A01 – Exercício 001

---
+++ Modulo_01_Estruturas_Basicas/M01A01–Ex001.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <locale.h>
+
+void main(){
+    setlocale(LC_ALL,"Portuguese"); //Define a lingua para Portugês
+
+}
+/*
+<<<< EX001 - Listagem >>>>
+
+Listagem de Alunos
+Nome            Nota
+-----------------------
+Ana Beatriz     8.5
+Bianca Martins  9.0
+Cláudio Sá      5.5
+Giovana Silva   7.5
+*/
