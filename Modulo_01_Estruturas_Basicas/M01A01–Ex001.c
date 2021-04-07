99c7da56faa61b01c9dfb221c79bc0b46f7d667a
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-07 18:46:20 +0100

    M01A01 – Exercício 001

--- Modulo_01_Estruturas_Basicas/M01A01–Ex001.c
+++ Modulo_01_Estruturas_Basicas/M01A01–Ex001.c
@@ -7,6 +7,11 @@ void main(){
     printf("Listagem de Alunos \n");
     printf("Nome \t\tNota \n");
     printf("-------------------- \n");
+    printf("Ana Beatriz \t8.5 \n");
+    printf("Bianca Martins \t9.0 \n");
+    printf("Cláudio Sá \t5.5 \n");
+    printf("Giovana Silva  \t7.5 \n");
+
 }
 /*
 <<<< EX001 - Listagem >>>>
