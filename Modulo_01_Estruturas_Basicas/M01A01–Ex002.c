a87a46cff9365f88c8ee846af39e81d410dfa182
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-07 18:48:06 +0100

    M01A01 – Exercício 002

--- Modulo_01_Estruturas_Basicas/M01A01–Ex002.c
+++ Modulo_01_Estruturas_Basicas/M01A01–Ex002.c
@@ -4,3 +4,16 @@
 void main(){
     setlocale(LC_ALL,"Portuguese"); //Define a lingua para Portugês
 }
+
+/*
+<<<< EX002 - Especiais >>>>
+
+=.=.=.=.=.=.=.=.=.=.=.=.=.=.=
+\a  =   Beep
+\n  =   Nova Linha
+\t  =   Tabulação
+\\  =   Barra
+%%  =   Percentagem
+\?  =   Interrogação
+=.=.=.=.=.=.=.=.=.=.=.=.=.=.=
+*/
