579dffae36a7e73aad023ebc3d82d6af1de0d62d
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-09 08:42:43 +0100

    M01A06 – Constantes

--- Modulo_01_Estruturas_Basicas/M01A06–Constantes.c
+++ Modulo_01_Estruturas_Basicas/M01A06–Constantes.c
@@ -5,9 +5,12 @@ void main(){
     setlocale(LC_ALL,"Portuguese");
 
     int total = 8; //variavel
-    //int IDADEVOTAR = 18;
+    const int IDADEVOTAR = 18; //Constante
 
     printf("%d",total);
     total = 10;
     printf("\n%d", total);
+
+    printf("\n\n");
+    printf("A idade para votar %d anos", IDADEVOTAR);
 }
