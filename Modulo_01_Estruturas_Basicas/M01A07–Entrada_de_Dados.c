e5a2936a354283c51e218a12d69e631d4a0b316e
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-09 09:16:42 +0100

    M01A07 – Entrada de Dados

--- Modulo_01_Estruturas_Basicas/M01A07–Entrada_de_Dados.c
+++ Modulo_01_Estruturas_Basicas/M01A07–Entrada_de_Dados.c
@@ -4,19 +4,19 @@
 void main(){
     //setlocale(LC_ALL,"Portuguese");
 
-    int n;
-    float m;
-    printf("Digite um número inteiro: ");
-    scanf("%d",&n);
-    printf("Digite um número real: ");
-    scanf("%f",&m);
-    printf("Você acabou de digitar os valores %d e %.2f. Obrigado!\n", n, m);
+    //int n;
+    //float m;
+    //printf("Digite um numero inteiro: ");
+    //scanf("%d",&n);
+    //printf("Digite um numero real: ");
+    //scanf("%f",&m);
+    //printf("Voce acabou de digitar os valores %d e %.2f. Obrigado!\n", n, m);
 
     //Ler caracteres
     char r;
     printf("Digite uma letra: ");
     scanf("%c",&r);
-    printf("Você digitou a letra %c", r);
+    printf("Voce digitou a letra \"%c\"", r);
 }
 
 /*  Lendo números
