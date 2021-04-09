d0591c91f1b331f6eac4303aeea6074a5f781064
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-09 09:48:30 +0100

    M01A08 – Exercícios em Linguagem C

--- Modulo_01_Estruturas_Basicas/M01A08–Exercicios_em_Linguagem_C.c
+++ Modulo_01_Estruturas_Basicas/M01A08–Exercicios_em_Linguagem_C.c
@@ -1,12 +1,33 @@
 /* <<< EX003 - Dados >>>
 
 Qual é o seu nome? Gustavo Guanabara
-Quantos anos voce tem? 39
-Qual e o seu peso? (kg) 97.2
+Quantos anos você tem? 39
+Qual é o seu peso? (kg) 97,2
 
 --------<<< PROCESSANDO >>>--------
 Muito prazer, Gustavo Guanabara.
-Voce tem 39 anos e pesa 97.00Kg correcto?
+Você tem 39 anos e pesa 97.00Kg correcto?
 FIM.
 */
 
+
+/* <<< EX004 - Listagem >>>
+
+ Cadastrando a primeira pessoa:
+ -------------------------------------
+ NOME: Gustavo
+ SEXO [M/F]: M
+ NOTA: 4,5
+
+  Cadastrando a segunda pessoa:
+ -------------------------------------
+ NOME: Maria da Silva
+ SEXO [M/F]: F
+ NOTA: 9,5
+
+   Cadastrando a terceira pessoa:
+ -------------------------------------
+ NOME: Claudio Peçanha
+ SEXO [M/F]: M
+ NOTA: 10
+*/
