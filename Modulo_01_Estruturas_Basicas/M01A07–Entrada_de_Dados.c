a1bdb0a1d32314d43ac8195fa77ef276065ba673
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-09 09:37:54 +0100

    M01A07 – Entrada de Dados

--- Modulo_01_Estruturas_Basicas/M01A07–Entrada_de_Dados.c
+++ Modulo_01_Estruturas_Basicas/M01A07–Entrada_de_Dados.c
@@ -5,31 +5,30 @@ void main(){
     //setlocale(LC_ALL,"Portuguese");
 
     //Ler números inteiros
-    //int n;
-    //float m;
-    //printf("Digite um numero inteiro: ");
-    //scanf("%d",&n);
-    //printf("Digite um numero real: ");
-    //scanf("%f",&m);
-    //printf("Voce acabou de digitar os valores %d e %.2f. Obrigado!\n", n, m);
+    int n;
+    float m;
+    printf("Digite um numero inteiro: ");
+    scanf("%d",&n);
+    printf("Digite um numero real: ");
+    scanf("%f",&m);
+    printf("Voce acabou de digitar os valores %d e %.2f. Obrigado!\n", n, m);
 
     //Ler caracteres
-    //char r;
-    //char s;
-    //printf("Digite uma letra: ");
-    //fflush(stdin); //Limpa o buffer do teclado
-        //scanf("%c",&r);
-    //r = getchar();
-    //printf("Digite outra letra: ");
-    //fflush(stdin); //Limpa o buffer do teclado
-        //scanf("%c",&s);
-    //s = getchar();
-    //printf("Voce digitou as letra \"%c\" e \"%c\"", r, s);
+    char r;
+    char s;
+    printf("\nDigite uma letra: ");
+    fflush(stdin); //Limpa o buffer do teclado
+    r = getchar();
+    printf("Digite outra letra: ");
+    fflush(stdin); //Limpa o buffer do teclado
+    s = getchar();
+    printf("Voce digitou as letra \"%c\" e \"%c\"", r, s);
 
     //Ler Strings
     char nome[30];
     char endereco[40];
 
+    printf("\n\n");
     printf("Digite o seu nome: ");
     fflush(stdin); //Limpa o buffer do teclado
     gets(nome); //leitura de string
