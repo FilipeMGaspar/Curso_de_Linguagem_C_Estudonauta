aad57cc3fe533c25d9fd9bbdccf953a4f56173f8
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-11 19:03:59 +0100

    M01A01 – Ex005

---
+++ Modulo_01_Estruturas_Basicas/M01A09–EX005.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+#include <time.h>
+
+void main(){
+    setlocale(LC_ALL,"Portuguese");
+
+    srand(time(NULL));//Utiliza o relógio do sistema para gerar número aleatórios
+    int gerado = rand() % 5 + 1;//Gera entre 0 e 5
+}
+
+/* <<< EX005 - Será que vocè acerta? >>>
+
+    Vou pensar num número entre 1 e 5.
+    Tente adivinhar!
+    Qual é o seu paçpite? 2
+    Eu pensei no número 3 o seu palpite foi o número 2.
+*/
