1377f79115a4ec4db9d4c5e132536a1e57fb0306
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-11 18:54:21 +0100

    M01A09 – Números Aleatórios

--- Modulo_01_Estruturas_Basicas/M01A09–Numeros_Aleatorios.c
+++ Modulo_01_Estruturas_Basicas/M01A09–Numeros_Aleatorios.c
@@ -7,6 +7,6 @@ void main(){
     setlocale(LC_ALL,"Portuguese");
 
     srand(time(NULL));
-    int n =rand();
+    int n = rand() % 20 + 1;//Gera entre 0 e 10
     printf("Eu gerei o numero (%d)", n);
 }
---
+++ Modulo_01_Estruturas_Basicas/M01A09–Numeros_Aleatorios_EXERCICIO.c
@@ -0,0 +1,7 @@
+/* <<< EX005 - Será que vocè acerta? >>>
+
+    Vou pensar num número entre 1 e 5.
+    Tente adivinhar!
+    Qual é o seu paçpite? 2
+    Eu pensei no número 3 o seu palpite foi o número 2.
+*/
