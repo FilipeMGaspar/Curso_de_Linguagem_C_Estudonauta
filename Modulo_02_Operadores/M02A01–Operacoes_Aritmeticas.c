aeac767497f19e3232fc9520da70159a45f8da57
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-12 19:13:24 +0100

    M02A01 – Operações Aritméticas

--- Modulo_02_Operadores/M02A01–Operacoes_Aritmeticas.c
+++ Modulo_02_Operadores/M02A01–Operacoes_Aritmeticas.c
@@ -4,27 +4,27 @@
 void main(){
     //setlocale(LC_ALL,"Portuguese");
 
+    char l = 'J';
+    printf("Estou com a letra %d na memoria \n",l);
+    printf("\n\n");
+
     float preco = 1000;
     float desc = (preco * 10 /100);
-
     printf("Preco Original: %.2f \n", preco);
     printf("O valor do desconto e: %.2f \n", desc);
     printf("Preco final: %.2f \n", (preco-desc));
 
-    /*float n1 = 5.5;
+    printf("\n\n");
+    float n1 = 5.5;
     float n2 = 8.5;
-
     float m = (n1+ n2) /2;
     printf("A media e de %.2f", m);
-    */
 
-   /*
+    printf("\n\n");
     int a = 3;
     int b = 2;
     int r = a % b;
-
-    printf("O resultado e %d", r);
-    */
+    printf("O resultado e %d \n", r);
 }
 
 /*Operadores Aritméticos
