ecdd9b860c65a170abb7cfa29f973d0b3f47be54
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-12 19:19:33 +0100

    M02A01 – EXERCICIOS Operações Aritméticas

--- Modulo_02_Operadores/M02A01–EXERCICIOS.c
+++ Modulo_02_Operadores/M02A01–EXERCICIOS.c
@@ -5,3 +5,9 @@
     Analisando o número 7, seu antecessor é 6 e seu sucessor é 8.
 */
 
+/* <<< EX007 - Dobro e Terça parte >>>
+
+    Digite um número: 9
+
+     Analisando o número 9, seu dobro é 18 e sua terça parte é 3.
+*/
