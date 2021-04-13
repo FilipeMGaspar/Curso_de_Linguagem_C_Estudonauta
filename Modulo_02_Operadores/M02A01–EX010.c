ed65bb3dc272433f85d3d2ca9fc6399725b80f4b
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-13 17:25:50 +0100

    M02A01 – EX010

--- Modulo_02_Operadores/M02A01–EX010.c
+++ Modulo_02_Operadores/M02A01–EX010.c
@@ -4,4 +4,14 @@
 void main(){
     setlocale(LC_ALL, "Portoguese");
 
+    printf("<<< EX010 -  Preço do Produto>>> \n\n");
 }
+
+/*<<< EX010 -  Preço do Produto>>>
+
+    Produto: Iphone
+    Preço de Iphone: R$ 7500
+    Desconto: (%) 12
+    O produto iPhone custava R$ 7500, mas com 12,00% de desconto, passa a custar R$ 6600,00.
+
+*/
