ef5b688e6d0a767f6bc589732d988bd622ee22d9
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-13 17:14:59 +0100

    M02A01 – EX009

--- Modulo_02_Operadores/M02A01–EX009.c
+++ Modulo_02_Operadores/M02A01–EX009.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#include <locale.h>
 
+void main(){
+    setlocale(LC_ALL, "Portoguese");
+
+}
+
+/* <<< EX009 -  Média do aluno>>>
+
+    Nome do Aluno: Gustavo
+    Nota 1: 4,5
+    Nota 2: 8
+    O aluno gustavo tirou notas 4,5 e 8, ficou com média de 6,3
+*/
