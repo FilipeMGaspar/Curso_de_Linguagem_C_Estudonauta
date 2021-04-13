3be68e6130867314ff3ce9c75359d8d9348087c5
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-13 17:03:19 +0100

    M02A01 – EX008

---
+++ Modulo_02_Operadores/M02A01–EX008.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <locale.h>
+
+void main(){
+    setlocale(LC_ALL, "Portoguese");
+
+
+}
+
+/* <<< EX008 - Alfabeto >>>
+    Digite uma letra: r
+
+    Antes da letra R temos a tera Q. depois temos a letra S.
+*/
