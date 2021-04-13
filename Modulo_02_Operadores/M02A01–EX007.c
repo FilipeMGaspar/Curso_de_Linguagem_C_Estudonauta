f313feec21a040e03525a94b77760ca158d455ac
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-13 16:59:39 +0100

    M02A01 – EX007

--- Modulo_02_Operadores/M02A01–EX007.c
+++ Modulo_02_Operadores/M02A01–EX007.c
@@ -7,6 +7,8 @@ void main(){
 
     printf("<<< EX007 - Dobro e Terça parte >>> \n\n");
     printf("Digite um número: ");
+    scanf("%d", &num);
+    printf("Analisando o número %d, seu dobro é %d e sua terça parte é %d.");
 }
 
 /* <<< EX007 - Dobro e Terça parte >>>
