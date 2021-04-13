61b039dd27b29bf60bf60e3b2e6b2aa1b4b4716e
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-13 18:22:14 +0100

    M02A01 – EX010

--- Modulo_02_Operadores/M02A01–EX010.c
+++ Modulo_02_Operadores/M02A01–EX010.c
@@ -15,7 +15,7 @@ void main(){
     printf("Desconto: (%%) ");
     scanf("%f",&desconto);
     preco_final = preco - (preco * desconto / 100);
-
+    printf("O produto %s custava R$ %.2f, mas com %.2f de desconto, passa a custar R$ %.2f. \n", nome_prod, preco, desconto, preco_final);
 }
 
 /*<<< EX010 -  Preço do Produto>>>
