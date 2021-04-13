022d0980a48e7eadb4736461a6f99d10e7a15224
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-13 16:49:48 +0100

    M02A01 – EX006

---
+++ Modulo_02_Operadores/M02A01–EX006.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <locale.h>
+
+void main(){
+    //setlocale(LC_ALL,"Portuguese");
+
+}
+/* <<< EX006 - Antecessor e Sucessor >>>
+
+    Digite um número: 7
+
+    Analisando o número 7, seu antecessor é 6 e seu sucessor é 8.
+*/
