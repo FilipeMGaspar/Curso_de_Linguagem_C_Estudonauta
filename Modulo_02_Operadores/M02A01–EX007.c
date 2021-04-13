a7c054da614b6d354e4ff88d5e41b3fc5edce3c4
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-13 16:56:31 +0100

    M02A01 – EX007

---
+++ Modulo_02_Operadores/M02A01–EX007.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <locale.h>
+
+void main(){
+    setlocale(LC_ALL,"Portuguese");
+
+}
+
+/* <<< EX007 - Dobro e Terça parte >>>
+
+    Digite um número: 9
+
+     Analisando o número 9, seu dobro é 18 e sua terça parte é 3.
+*/
