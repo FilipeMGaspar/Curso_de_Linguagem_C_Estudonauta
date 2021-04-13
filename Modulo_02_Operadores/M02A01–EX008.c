d4f67a5383e6a345fb53a71fc81191eba7475d55
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-13 17:12:35 +0100

    M02A01 – EX008

--- Modulo_02_Operadores/M02A01–EX008.c
+++ Modulo_02_Operadores/M02A01–EX008.c
@@ -9,7 +9,7 @@ void main(){
     printf("Digite uma letra: ");
     letra = getchar();
 
-    printf("Antes da letra %c temos a letra %c. Depois temos a letra %c \n", letra, (letra-1), (letra+1));
+    printf("Antes da letra %c temos a letra %c. Depois temos a letra %c. \n", letra, (letra-1), (letra+1));
 }
 
 /* <<< EX008 - Alfabeto >>>
