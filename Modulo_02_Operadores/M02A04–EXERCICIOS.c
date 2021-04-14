5c53f8558208fd1c485667e5bc8432df298a224d
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-14 19:38:50 +0100

    M02A04 – Exercícios :  Operador Ternário

--- Modulo_02_Operadores/M02A04–EXERCICIOS.c
+++ Modulo_02_Operadores/M02A04–EXERCICIOS.c
@@ -6,3 +6,16 @@ Digite um número qualquer: 7
 O número que você digitou é ÍMPAR!
 
 */
+
+/*
+<<< EX012 - Situação do Aluno >>>
+
+Primeira nota: 5,5
+Segunda nota: 8,5
+
+A média do aluno foi 7,0
+A sua sitação é: APROVADO
+
+
+
+*/
