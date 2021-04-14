e0cf19274669e7f79031137a74d535b9f6628d52
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-14 19:08:04 +0100

    M02A04 – Operador Ternário

--- Modulo_02_Operadores/M02A04–Operador_Ternario_02.c
+++ Modulo_02_Operadores/M02A04–Operador_Ternario_02.c
@@ -3,6 +3,12 @@
 
 void main(){
     setlocale(LC_ALL, "Portuguese");
+    int n1, n2;
 
+    printf("Digite um número: ");
+    scnaf("%d", &n1);
+    printf("Digite outro número: ");
+    scanf("%d", &n2);
 
+    int maior = (n1 > n2) ? n1 : n2;
 }
