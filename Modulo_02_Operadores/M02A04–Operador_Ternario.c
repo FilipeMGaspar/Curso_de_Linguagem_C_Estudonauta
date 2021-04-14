0f1fbbab1971cdc520453eb49fb84c963a50f4a1
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-14 18:58:28 +0100

    M02A04 – Operador Ternário

--- Modulo_02_Operadores/M02A04–Operador_Ternario.c
+++ Modulo_02_Operadores/M02A04–Operador_Ternario.c
@@ -2,14 +2,15 @@
 #include <locale.h>
 
 void main(){
-    setlocale(LC_ALL, "Portuguese");
+    //setlocale(LC_ALL, "Portuguese");
 
+    int a = 14, b = 8;
+    int c = (a>b) ? 1:2;
+    printf("O resultado e: %d \n", c);
 
 
     /*
      c = (x>0)?4:8
-     c variavel
-     = condição ?
-     valor se verdadeiro : valor se falso
+    variavel = condição ?  valor se verdadeiro : valor se falso
     */
 }
