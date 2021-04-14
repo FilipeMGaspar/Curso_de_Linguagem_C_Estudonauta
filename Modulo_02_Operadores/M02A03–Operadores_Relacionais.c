42f0901697736fc90b5d423133960794614149d5
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-14 18:49:54 +0100

    M02A03 – Operadores Relacionais

--- Modulo_02_Operadores/M02A03–Operadores_Relacionais.c
+++ Modulo_02_Operadores/M02A03–Operadores_Relacionais.c
@@ -4,9 +4,13 @@
 void main(){
     setlocale(LC_ALL, "Portuguese");
 
-    int a = 6;
+    int a = 8;
     int b = 6;
-    int c = (a==b);
+    int c = (a>=b);
 
     printf("O resultado e %d \n", c);
 }
+/*
+true = 1
+false = 0
+*/
