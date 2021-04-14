cb50410a6f6d0dfb4f8af38920635090c6523751
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-14 19:33:49 +0100

    M02A04 – Operador Ternário

--- Modulo_02_Operadores/M02A04–Operador_Ternario_02.c
+++ Modulo_02_Operadores/M02A04–Operador_Ternario_02.c
@@ -6,9 +6,10 @@ void main(){
     int n1, n2;
 
     printf("Digite um número: ");
-    scnaf("%d", &n1);
+    scanf("%d", &n1);
     printf("Digite outro número: ");
     scanf("%d", &n2);
 
     int maior = (n1 > n2) ? n1 : n2;
+    printf("Entre %d e %d, o maior foi %d \n", n1, n2, maior);
 }
