63ac94be0446aba62b7b712555d0d7deada1674e
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-15 20:08:18 +0100

    M02A04 - EX011

---
+++ Modulo_02_Operadores/M02A01–EX011.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <locale.h>
+
+void main(){
+    setlocale(LC_ALL, "Portuguese");
+
+}
+/*
+<<< EX011 - Par ou Ímpar >>>
+Digite um número qualquer: 7
+O número que você digitou é ÍMPAR!
+*/
