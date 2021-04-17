15383910a6d0a1050058a9ea6bd611f93859163a
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-17 17:29:45 +0100

    M02A05 – Operações com Strings

--- Modulo_02_Operadores/M02A05–Operacoes_com_Strings.c
+++ Modulo_02_Operadores/M02A05–Operacoes_com_Strings.c
@@ -5,7 +5,7 @@
 void main(){
     setlocale(LC_ALL, "Portuguese");
 
-   char nome[] = "Gustavo";
+   char nome[] = "André";
    printf("Original: %s \n", nome);
    printf("Maiúsculas: %s \n", strupr(nome));
    printf("Minusculas: %s \n", strlwr(nome));
