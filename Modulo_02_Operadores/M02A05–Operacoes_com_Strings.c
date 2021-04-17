789fb25b63fc658497e31859d05aebfc361f75aa
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-17 17:36:38 +0100

    M02A05 – Operações com Strings

--- Modulo_02_Operadores/M02A05–Operacoes_com_Strings.c
+++ Modulo_02_Operadores/M02A05–Operacoes_com_Strings.c
@@ -5,28 +5,30 @@
 void main(){
     setlocale(LC_ALL, "Portuguese");
 
-   char nome[] = "André";
-   printf("Original: %s \n", nome);
-   printf("Maiúsculas: %s \n", strupr(nome));
-   printf("Minusculas: %s \n", strlwr(nome));
+    char name[] = "André";
+    printf("Original: %s \n", name);
+    printf("Maiúsculas: %s \n", strupr(name));
+    printf("Minusculas: %s \n", strlwr(name));
 
-   /*
-   char s1[] = "Gustavo ";
-   char s2[] = "Guanabara";
-   printf("%s \n", strcat(s1, s2));
-   */
+    printf("\n");
+    printf("Concatenação de Strings \n");
+    char s1[] = "Gustavo ";
+    char s2[] = "Guanabara";
+    printf("%s \n", strcat(s1, s2));
 
-   /*
-    char s1[] = "Guanabara";
-    char s2[] = "Fiuza da Silva";
-    int res = strcmp(s1, s2);
+    printf("\n");
+    printf("Comparação entre strings \n");
+    char st1[] = "Guanabara";
+    char st2[] = "Fiuza da Silva";
+    int res = strcmp(st1, st2);
     printf("O resultado da comparação e %d \n", res);
-    */
-    /*
+
+    printf("\n");
+    printf("Tamanho de uma string \n");
     char nome[] = "Gustavo";
     int tam = strlen(nome);
     printf("o nome %s tem %d letras \n", nome, tam);
-    */
+
 
    /*
    char sit[10];
