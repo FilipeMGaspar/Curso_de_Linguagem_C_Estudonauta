7ba765ca49652adfe38ef7505d4b1e297ddd41c3
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-17 16:56:26 +0100

    M02A05 – Operações com Strings

--- Modulo_02_Operadores/M02A05–Operacoes_com_Strings.c
+++ Modulo_02_Operadores/M02A05–Operacoes_com_Strings.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <locale.h>
+#include <string.h> //Para poder trabalhar com strings
 
 void main(){
     setlocale(LC_ALL, "Portuguese");
 
     char nome[10];
 
-    nome = "Gustavo";
-    printf("%s", nome);
+    //nome = "Gustavo"; //Gerou um erro
+    strcpy(nome, "Gustavo");
+    printf("Muito prazer em te conhecer, %s!", nome);
 }
