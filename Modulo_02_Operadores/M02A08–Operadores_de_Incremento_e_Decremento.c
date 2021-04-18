7e3a7d0da0f03e4728c8231f5c849702694fc034
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-18 09:19:28 +0100

    M02A08 – Operadores de Incremento e Decremento

--- Modulo_02_Operadores/M02A08–Operadores_de_Incremento_e_Decremento.c
+++ Modulo_02_Operadores/M02A08–Operadores_de_Incremento_e_Decremento.c
@@ -4,11 +4,14 @@
 void main(){
     setlocale(LC_ALL,"Portuguese");
     int n = 8;
-    n--;
-    printf("O resultado é %d \n", n);
+    //n--;
+    printf("O resultado é %d \n", ++n);
 }
 
 /*
- x = x + 1; ou x += 1 ou x++;
- y = y - 1; ou y -= 1 ou y--
+ x = x + 1; ou x += 1 ou x++; posincremento
+ y = y - 1; ou y -= 1 ou y-- posdecremento
+
+ ++x preincremento
+ --y predecremento
 */
