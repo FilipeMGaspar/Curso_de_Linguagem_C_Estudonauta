eb7c1a387ba09faf9ad440254f80bdcc75b4691f
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-18 08:55:05 +0100

    M02A06 – Operadores Lógicos

--- Modulo_02_Operadores/M02A06–Operadores_Logicos.c
+++ Modulo_02_Operadores/M02A06–Operadores_Logicos.c
@@ -9,6 +9,15 @@ void main(){
     printf("%d > %d E %d > %d : %s \n",a ,b ,a ,c ,(a>b && a>c) ? "Verdadeiro" : "Falso");
     printf("\nDisjunção \n");
     printf("%d > %d OU %d > %d : %s \n",a ,b ,a ,c ,(a>b || a>c) ? "Verdadeiro" : "Falso");
+
+    printf("\nExemplo 2:\n");
+    a = 5;
+    b = 4;
+    c = 2;
+    printf("%s \n",((a > b || a < c && !c!=4) ? "Verdadeiro" : "Falso"));
+    //                 V   OU   F   && NÃO V
+    //                      V       &&   F
+    //                               F
 }
 /*
 &&  Operador de conjunção e
