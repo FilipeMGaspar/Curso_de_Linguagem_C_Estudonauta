ef333cb932e72361fa2d916fcfd89fb929c4f9af
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-21 18:42:48 +0100

    M02A11 – Operadores Bit a Bit

---
+++ Modulo_02_Operadores/M02A11–Operadores_Bit_a_Bit_EXERCICIO.c
@@ -0,0 +1,13 @@
+/*
+<<< EX013 - Operadores Bitwise >>>
+
+Digite o primeiro valor: 25
+Digite o segundo valor: 12
+
+
+------------- OPERAÇÔES BITWISE --------------
+Calculado 25 & 12 é igual a 8
+Calculado 25 | 12 é igual a 29
+Calculado 25 ^ 12 é igual a 21
+
+*/
