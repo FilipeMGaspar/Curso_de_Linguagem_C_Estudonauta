f6c285ef2ce32dfaf4a635e2efd03e9e4880d472
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-21 19:50:46 +0100

    M02A11 – EX013

--- Modulo_02_Operadores/M02A11–EX013.c
+++ Modulo_02_Operadores/M02A11–EX013.c
@@ -8,10 +8,10 @@ void main(){
     int n1, n2, r;
     printf("Digite o primeiro número: ");
     fflush(stdin);
-    scanf("%d",$n1);
+    scanf("%d",&n1);
     printf("Digite o segundo número: ");
     fflush(stdin);
-    scanf("%d",$n2);
+    scanf("%d",&n2);
 
     printf("\n\n");
     printf("------ OPERAÇÕES BITWISE ------ \n");
