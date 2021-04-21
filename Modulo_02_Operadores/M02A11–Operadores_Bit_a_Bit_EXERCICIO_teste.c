96f837ff28dc008bd37091b6285426108ec6a232
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-21 19:26:02 +0100

    M02A11 – Operadores Bit a Bit

--- Modulo_02_Operadores/M02A11–Operadores_Bit_a_Bit_EXERCICIO_teste.c
+++ Modulo_02_Operadores/M02A11–Operadores_Bit_a_Bit_EXERCICIO_teste.c
@@ -4,6 +4,14 @@
 void main(){
     setlocale(LC_ALL,"Portuguese");
     int a = 15, b =5;
-    int n = a & b;
+    int n;
+    n = a & b;
     printf("%d & %d = %d\n", a, b, n);
+    printf("\n");
+
+    a = 27;
+    b = 17;
+    n = a | b;
+    printf("%d | %d = %d\n", a, b, n);
+
 }
