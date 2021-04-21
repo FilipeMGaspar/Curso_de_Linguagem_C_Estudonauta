b3d32245c81157665b793b176653cbb7e770bb01
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-21 19:46:24 +0100

    M02A11 – EX013

--- Modulo_02_Operadores/M02A11–EX013.c
+++ Modulo_02_Operadores/M02A11–EX013.c
@@ -3,5 +3,22 @@
 
 void main(){
     setlocale(LC_ALL,"Portuguese");
+    printf("<<< EX013 - Operadores Bitwise >>> \n\n");
 
+    int n1, n2;
+    printf();
 }
+
+/*
+<<< EX013 - Operadores Bitwise >>>
+
+Digite o primeiro valor: 25
+Digite o segundo valor: 12
+
+
+------------- OPERAÇÔES BITWISE --------------
+Calculado 25 & 12 é igual a 8
+Calculado 25 | 12 é igual a 29
+Calculado 25 ^ 12 é igual a 21
+
+*/
