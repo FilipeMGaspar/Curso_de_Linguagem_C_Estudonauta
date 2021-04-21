9894cd0d73818960d98ac03aa52987475de249c7
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-21 18:26:02 +0100

    M02A11 – Operadores Bit a Bit

--- Modulo_02_Operadores/M02A11–Operadores_Bit_a_Bit.c
+++ Modulo_02_Operadores/M02A11–Operadores_Bit_a_Bit.c
@@ -3,6 +3,13 @@
 
 void main(){
     setlocale(LC_ALL,"Portuguese");
-    int n = 25 $ 12;
+    int n = 25 ^ 12;
     printf("O resultado foi %d \n",n);
 }
+// int n = 25 & 12;  dá como resultado 8
+
+
+//    int n = 25 | 12; dá como resultado 29
+
+
+//  int n = 25 ^ 12;  dá como resultado 21
