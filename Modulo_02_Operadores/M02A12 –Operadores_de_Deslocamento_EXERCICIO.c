0c7cb8734c3dabe65d9e2bfea9f11f329f2a592e
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-22 19:51:38 +0100

    M02A12 – Operadores de Deslocamento

---
+++ Modulo_02_Operadores/M02A12 –Operadores_de_Deslocamento_EXERCICIO.c
@@ -0,0 +1,12 @@
+/*
+<<< EX014 - Operadores de Deslocamento >>>
+
+Digite um número: 18
+Digite o deslocamento: 3
+
+----- OPERAÇÕES SHIDT -----
+Calculando 18 >> 3 é igual a 2
+Calculando 18 << 3 é igual a 144
+
+
+*/
