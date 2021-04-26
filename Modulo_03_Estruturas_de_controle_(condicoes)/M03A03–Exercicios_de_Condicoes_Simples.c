bbcbcf0a6806c18c5c6c6901c74e6fc86ff58174
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-26 12:56:52 +0100

    M03A03 – Exercícios de Condições Simples

--- Modulo_03_Estruturas_de_controle_(condicoes)/M03A03–Exercicios_de_Condicoes_Simples.c
+++ Modulo_03_Estruturas_de_controle_(condicoes)/M03A03–Exercicios_de_Condicoes_Simples.c
@@ -38,5 +38,23 @@ Volte Sempre!
 /*
 <<< EX017 - Fila de Banco >>>
 
-Em que ano você nasceu?
+Em que ano você nasceu? 1978
+
+----------------------------------------
+Você tem 40 anos, certo?
+Seja bem vindo(a) ao Banco Estudonauta!
+----------------------------------------
+
+se a idade doi maior ou igual a 65
+
+Em que ano você nasceu? 1930
+
+----------------------------------------
+Você tem 91 anos, certo?
+Seja bem vindo(a) ao Banco Estudonauta!
+
+=== ATENÇÃO! DIRIJA-SE PARA A FILA PREFERENCIAL ===
+----------------------------------------
+
+
 */
