eccd4ff66f4bf455696aa2d18c4952130541ce51
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-26 12:29:42 +0100

    M03A02 – Capturando a Data e Hora do Sistema

--- Modulo_03_Estruturas_de_controle_(condicoes)/M03A02–Capturando_a_Data_e_Hora_do_Sistema.c
+++ Modulo_03_Estruturas_de_controle_(condicoes)/M03A02–Capturando_a_Data_e_Hora_do_Sistema.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <locale.h>
+#include <time.h>
 
 void main(){
     setlocale(LC_ALL,"Portuguese");
+
+    time_t t;
+    time(&t);
+    struct tm*data;// tm nome do struct data ponteiro para data
+    data = localtime(&t);
+
 }
