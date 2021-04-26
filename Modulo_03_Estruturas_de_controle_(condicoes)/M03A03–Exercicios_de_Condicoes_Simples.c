3b0c27bac97272f27519df163e749673c02b43d5
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-26 12:53:07 +0100

    M03A03 – Exercícios de Condições Simples

---
+++ Modulo_03_Estruturas_de_controle_(condicoes)/M03A03–Exercicios_de_Condicoes_Simples.c
@@ -0,0 +1,35 @@
+/*
+ <<< EX015 - Bons alunos merecem parabéns >>>
+
+ Digite a sua primeira nota: 5.5
+
+ Digite a sua segunda nota 9.5
+
+ -----------------------------------------
+ MEUS PARABÉNS! A sua média final foi 7.0
+ -----------------------------------------
+
+ Só dá parabéns se a média for maior ou igual a 7
+*/
+
+/*
+<<< EX016 - Consumidor ganha desconto >>>
+
+Qual foi o valor total das compras? R$ 250
+
+--------------------------------------
+você comprou R$ 250,00 na nossa loja.
+Volte Sempre!
+--------------------------------------
+
+Se valor de compras for maior ou igual a 500 dá é 10%
+
+--------------------------------------
+você comprou R$ 800,00 na nossa loja.
+
+======= ATENÇÃO =======
+Por fazer mais de R$ 500 em compras, você vai receber R$ 80,00 de desconto
+O valor a ser pago é R$ 720!
+Volte Sempre!
+--------------------------------------
+*/
