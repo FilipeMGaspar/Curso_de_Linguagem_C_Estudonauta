f66d99387633237eb4aa3fa7bbc71cafff5edd65
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-26 12:41:19 +0100

    M03A02 – Capturando a Data e Hora do Sistema

--- Modulo_03_Estruturas_de_controle_(condicoes)/M03A02–Capturando_a_Data_e_Hora_do_Sistema.c
+++ Modulo_03_Estruturas_de_controle_(condicoes)/M03A02–Capturando_a_Data_e_Hora_do_Sistema.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 void main(){
-    setlocale(LC_ALL,"Portuguese");
+   // setlocale(LC_ALL,"Portuguese");
 
     time_t t;
     time(&t);
@@ -11,7 +11,16 @@ void main(){
     data = localtime(&t);
 
     int d = data->tm_mday; //Dia atual
-    int m = data->tm_mon + 1; // ME^s atual
-    int aaaa = data->tm_yday;
-    printf("Data atual: %d - %d - %d \n", d, m, aaaa);
+    int m = data->tm_mon + 1; // MÊs atual
+    int a = data->tm_year + 1900; //Ano atual que inicial em 1900
+    printf("Data atual: %d - %d - %d \n", d, m, a);
 }
+
+/*
+tm_mday    Dia do mês  de 1 a 31
+
+tm_mon     Mês de 0 a 11
+
+tm_year    Ano  inicia em 1900 necessita de ser somado
+
+*/
