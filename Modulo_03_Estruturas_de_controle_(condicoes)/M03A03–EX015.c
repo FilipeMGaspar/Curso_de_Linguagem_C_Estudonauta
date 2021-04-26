e9ad9654aa74cc7442c57d5d4110b556ac1e4af9
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-26 14:54:19 +0100

    M03A03 – EX015

--- Modulo_03_Estruturas_de_controle_(condicoes)/M03A03–EX015.c
+++ Modulo_03_Estruturas_de_controle_(condicoes)/M03A03–EX015.c
@@ -3,6 +3,20 @@
 
 void main(){
     setlocale(LC_ALL,"Portuguese");
-
+    printf(" <<< EX015 - Bons alunos merecem parabéns >>> \n\n");
 
 }
+
+/*
+ <<< EX015 - Bons alunos merecem parabéns >>>
+
+ Digite a sua primeira nota: 5.5
+
+ Digite a sua segunda nota 9.5
+
+ -----------------------------------------
+ MEUS PARABÉNS! A sua média final foi 7.0
+ -----------------------------------------
+
+ Só dá parabéns se a média for maior ou igual a 7
+*/
