62c56443362d34f7f3ca2d905b4a87744a466e5a
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-26 12:18:11 +0100

    M03A01 – Condições Simples

--- Modulo_03_Estruturas_de_controle_(condicoes)/M03A01–Condicoes_Simples.c
+++ Modulo_03_Estruturas_de_controle_(condicoes)/M03A01–Condicoes_Simples.c
@@ -3,4 +3,14 @@
 
 void main(){
     setlocale(LC_ALL,"Portuguese");
+
+    float vel;
+
+    printf("Digite a velocidade do carro: ");
+    fflush(stdin);
+    scnaf("%f",&vel);
+
+    if(){
+
+    }
 }
