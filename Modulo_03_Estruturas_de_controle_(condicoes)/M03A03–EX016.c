f786ac4b2ef1336ef91061f935a3afc6c319a423
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-27 11:08:23 +0100

    M03A03 – EX016

--- Modulo_03_Estruturas_de_controle_(condicoes)/M03A03–EX016.c
+++ Modulo_03_Estruturas_de_controle_(condicoes)/M03A03–EX016.c
@@ -4,3 +4,25 @@
 void main(){
     setlocale(LC_ALL,"Portuguese");
 }
+
+/*
+<<< EX016 - Consumidor ganha desconto >>>
+
+Qual foi o valor total das compras? R$ 250
+
+--------------------------------------
+você comprou R$ 250,00 na nossa loja.
+Volte Sempre!
+--------------------------------------
+
+Se valor de compras for maior ou igual a 500 dá é 10%
+
+--------------------------------------
+você comprou R$ 800,00 na nossa loja.
+
+======= ATENÇÃO =======
+Por fazer mais de R$ 500 em compras, você vai receber R$ 80,00 de desconto
+O valor a ser pago é R$ 720!
+Volte Sempre!
+--------------------------------------
+*/
