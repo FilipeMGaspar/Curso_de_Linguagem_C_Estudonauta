16122a1d6ffa2665a3dc5cb11a8836aa08df5778
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-28 13:18:14 +0100

    M03A05 – Exercícios com Condições Compostas

---
+++ Modulo_03_Estruturas_de_controle_(condicoes)/M03A05–Exercicios-com_Condicoes_Compostas.c
@@ -0,0 +1,21 @@
+/*
+<<< EX018 - Serviço Militar V1.0 >>>
+
+Atualmente estamos no ano de 2021
+
+Em que ano você nasceu? 1978
+
+---------------------------------------
+Sua idade atual é 40 anos.
+Já fez 18 anos. Espero sinceramente que você tenha se alistado.
+
+se idade < 18
+
+Em que ano você nasceu? 2010
+
+---------------------------------------
+Sua idade atual é 11 anos.
+Você ainda não tem 18 anos. Não pode se alistar.
+
+*/
+
