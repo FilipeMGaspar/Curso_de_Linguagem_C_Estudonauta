235af5410a24481fffa13f87bb2d19f4fe5049be
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-28 14:41:35 +0100

    M03A05 – EX020

--- Modulo_03_Estruturas_de_controle_(condicoes)/M03A05–EX020.c
+++ Modulo_03_Estruturas_de_controle_(condicoes)/M03A05–EX020.c
@@ -5,3 +5,22 @@ void main(){
     setlocale(LC_ALL,"Portuguese");
 
 }
+/*
+<<< EX020 - Preço da Passagem >>>
+
+======= TABELA DE PREÇOS =======
+Viagem até 200 Km   R$ 0.50/Km
+A partir de 200 Km   R$ 0.35/Km
+-----------------------------------
+
+Distância total da viagem, em Km: 100
+
+Uma viagem de 100,00 km vai custar R$ 0.50/km.
+Valor Total: R$ 50,00.
+
+Distância total da viagem, em Km: 500
+
+Uma viagem de 500,00 km vai custar R$ 0.35/km.
+Valor Total: R$ 175,00.
+
+*/
