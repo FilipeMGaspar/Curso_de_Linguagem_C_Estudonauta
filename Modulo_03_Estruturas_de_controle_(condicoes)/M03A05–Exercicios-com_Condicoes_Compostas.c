926826b6e9a0eaefce85f8afa101419adb4d25ce
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-28 13:21:21 +0100

    M03A05 – Exercícios com Condições Compostas

--- Modulo_03_Estruturas_de_controle_(condicoes)/M03A05–Exercicios-com_Condicoes_Compostas.c
+++ Modulo_03_Estruturas_de_controle_(condicoes)/M03A05–Exercicios-com_Condicoes_Compostas.c
@@ -19,3 +19,16 @@ Você ainda não tem 18 anos. Não pode se alistar.
 
 */
 
+/*
+<<< EX019 - Ano Bissexto >>>
+
+Digite um ano qualquer: 2016
+
+O ano de 2016 é BISSEXTO.
+
+
+
+Digite um ano qualquer: 2018
+
+O ano de 2018 NÃO É BISSEXTO.
+*/
