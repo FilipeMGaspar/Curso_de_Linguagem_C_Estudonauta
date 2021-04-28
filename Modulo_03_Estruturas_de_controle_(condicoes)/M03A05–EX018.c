de04cabdaf45ac8a3bfbc8b83fd1cb9775476bd2
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-28 14:16:35 +0100

    M03A05 – EX018

--- Modulo_03_Estruturas_de_controle_(condicoes)/M03A05–EX018.c
+++ Modulo_03_Estruturas_de_controle_(condicoes)/M03A05–EX018.c
@@ -1,7 +1,37 @@
 #include <stdio.h>
 #include <locale.h>
-
+#include <time.h>
 void main(){
     setlocale(LC_ALL,"Portuguese");
+    int ano_nascimento;
+    time_t t;
+    time(&t);
+    struct tm *data; //Ponteiro para tm
+    data = localtime(&t);
 
+
+    printf("<<< EX018 - Serviço Militar V1.0 >>> \n\n");
+
+    printf("Atualmente estamos no ano de %d \n\n",);
 }
+
+/*
+<<< EX018 - Serviço Militar V1.0 >>>
+
+Atualmente estamos no ano de 2021
+
+Em que ano você nasceu? 1978
+
+---------------------------------------
+Sua idade atual é 40 anos.
+Já fez 18 anos. Espero sinceramente que você tenha se alistado.
+
+se idade < 18
+
+Em que ano você nasceu? 2010
+
+---------------------------------------
+Sua idade atual é 11 anos.
+Você ainda não tem 18 anos. Não pode se alistar.
+
+*/
