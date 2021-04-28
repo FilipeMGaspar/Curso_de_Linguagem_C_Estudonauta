1f925a57a886b9843d8caf711a07c486453fb62e
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-28 14:23:54 +0100

    M03A05 – EX018

--- Modulo_03_Estruturas_de_controle_(condicoes)/M03A05–EX018.c
+++ Modulo_03_Estruturas_de_controle_(condicoes)/M03A05–EX018.c
@@ -21,9 +21,13 @@ void main(){
     idade = ano_atual - ano_nascimento;
     printf("\n-------------------------------------------------- \n");
     printf("A sua idade atual são %d anos. \n",idade);
+
     if(idade >= 18){
-       printf("Já fez 18 anos. \nEspero sinceramente que você tenha se alistado.\n");
+        printf("Já fez 18 anos. \nEspero sinceramente que você tenha se alistado.\n");
+    }else{
+        printf("Você ainda não tem 18 anos. \nNão pode se alistar.");
     }
+
     printf("-------------------------------------------------- \n");
 
 }
