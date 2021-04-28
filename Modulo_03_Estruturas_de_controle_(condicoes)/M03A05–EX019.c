a72653b3e66e9cfd0d8810502949cc388fe83585
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-28 14:37:29 +0100

    M03A05 – EX019

--- Modulo_03_Estruturas_de_controle_(condicoes)/M03A05–EX019.c
+++ Modulo_03_Estruturas_de_controle_(condicoes)/M03A05–EX019.c
@@ -11,10 +11,23 @@ void main(){
     scanf("%d",&ano);
     printf("\n---------------------------- \n");
 
-    printf("O ano de %d \n", ano);
-    printf("Resto da divisão de %d por 4 = %d ", ano,(ano%4) );
+    printf("O ano de %d ", ano);
+    if(ano % 4 == 0){
+        printf("É BISSEXTO");
+    }
+   // printf("Resto da divisão de %d por 4 = %d ", ano,(ano%4) );
     printf("\n---------------------------- \n");
 }
+
+// Regra utilizada:
+/*Para saber se um ano é bissexto basta dividi-lo por 4
+e conferir se o resultado é um número exato, por exemplo:
+
+1980/4 = 495 (é bissexto)  resto da divisão é 0
+
+2011/4 = 502,75 (não é bissexto) resto da divisão não é 0
+*/
+
 
 /*
 <<< EX019 - Ano Bissexto >>>
