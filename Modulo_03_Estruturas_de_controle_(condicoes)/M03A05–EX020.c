ac057fa54400ffcae7acd6c813f4c7ba562b859b
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-28 14:44:22 +0100

    M03A05 – EX020

--- Modulo_03_Estruturas_de_controle_(condicoes)/M03A05–EX020.c
+++ Modulo_03_Estruturas_de_controle_(condicoes)/M03A05–EX020.c
@@ -5,8 +5,10 @@ void main(){
     setlocale(LC_ALL,"Portuguese");
 
     printf("<<< EX020 - Preço da Passagem >>> \n\n");
-    printf("======= TABELA DE PREÇOS ======= \n");
-    printf("Viagem até 200 Km \tR$ 0.50/Km");
+    printf("========= TABELA DE PREÇOS ========= \n");
+    printf("Viagem até 200 Km \tR$ 0.50/Km \n");
+    printf("A partir de 200 km \tR$ 0.35/km \n");
+    printf("------------------------------------ \n");
 }
 /*
 <<< EX020 - Preço da Passagem >>>
