abc8a98fa8d6e0dbd780207474f2c721618ccdc2
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-28 13:08:06 +0100

    M03A04 – Condições Compostas

--- Modulo_03_Estruturas_de_controle_(condicoes)/M03A04–Condicoes_Compostas.c
+++ Modulo_03_Estruturas_de_controle_(condicoes)/M03A04–Condicoes_Compostas.c
@@ -8,5 +8,9 @@ void main(){
     printf("Digite um número: ");
     fflush(stdin);
     scanf("%d",&n);
-
+    if(n % 2 == 0){
+        printf("O número %d é PAR! \n", n);
+    }else{
+        printf("O número %d é ÍMPAR! \n", n);
+    }
 }
