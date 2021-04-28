d72410d403de6efde58abce60503e2384ecb4f13
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-28 14:27:04 +0100

    M03A05 – EX019

--- Modulo_03_Estruturas_de_controle_(condicoes)/M03A05–EX019.c
+++ Modulo_03_Estruturas_de_controle_(condicoes)/M03A05–EX019.c
@@ -5,3 +5,16 @@ void main(){
     setlocale(LC_ALL,"Portuguese");
 
 }
+
+/*
+<<< EX019 - Ano Bissexto >>>
+
+Digite um ano qualquer: 2016
+
+O ano de 2016 é BISSEXTO.
+
+
+Digite um ano qualquer: 2018
+
+O ano de 2018 NÃO É BISSEXTO.
+*/
