b7bb9f5c07d5658755da3d34decaa333409a99c5
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-29 14:44:13 +0100

    M03A05 – EX021

--- Modulo_03_Estruturas_de_controle_(condicoes)/M03A05–EX021.c
+++ Modulo_03_Estruturas_de_controle_(condicoes)/M03A05–EX021.c
@@ -5,3 +5,16 @@ void main(){
     setlocale(LC_ALL,"Portuguese");
 
 }
+
+/*
+<<< EX021 - Inverso ou oposto >>>
+
+Digite um número: 4
+
+O inverso de 4 é igual a 0,2500
+
+
+Digite um número: -8
+O oposto de -8 é igual a 8
+
+*/
