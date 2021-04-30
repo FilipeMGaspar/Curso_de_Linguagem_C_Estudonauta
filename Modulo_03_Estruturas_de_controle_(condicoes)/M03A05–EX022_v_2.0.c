f2e42138945633a73b090f6d1b82a4c0f6f44f3a
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-04-30 13:08:11 +0100

    M03A05 – EX022 V_2.0

--- Modulo_03_Estruturas_de_controle_(condicoes)/M03A05–EX022_v_2.0.c
+++ Modulo_03_Estruturas_de_controle_(condicoes)/M03A05–EX022_v_2.0.c
@@ -5,3 +5,27 @@ void main(){
     setlocale(LC_ALL,"Portuguese");
 
 }
+
+/*
+<<< EX022 - Dá para ver o filme? V 1.0>>>
+
+========= CINEMA ESTUDONAUTA =========
+HORÁRIO DO FILME: 19h - PREÇO DO INGRESSO: R$ 20
+-------------------------------------------------
+
+Quanto dinheiro você tem? R$ 10
+
+Agora são 17 horas.
+Infelizmente não é possivel comprar o ingresso.
+
+
+
+Quanto dinheiro você tem? R$ 50
+
+Agora são 17 horas.
+Você consegue comprar o ingresso.
+
+// O programa deve sofrer uma modificação pedindo a hora do filme
+// pergunta também quanto dinheiro tem
+// O Ingresso so pode ser comprado se tiver o dinheioro e estiver dentro do horário do filme
+*/
