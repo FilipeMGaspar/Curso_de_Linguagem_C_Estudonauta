1bcceb554444a7da48cb00b97f78fe142ce9d3e3
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-05-04 19:38:19 +0100

    M03A06 – Condições Aninhadas

--- Modulo_03_Estruturas_de_controle_(condicoes)/M03A06–Condicoes_Aninhadas.c
+++ Modulo_03_Estruturas_de_controle_(condicoes)/M03A06–Condicoes_Aninhadas.c
@@ -4,4 +4,22 @@
 void main(){
     setlocale(LC_ALL,"Portuguese");
 
+    int n1, n2;
+
+    printf("Digite um número: ");
+    fflush(stdin);
+    scanf("%d",&n1);
+    printf("Digite outro número: ");
+    fflush(stdin);
+    scanf("%d",&n2);
+
+    if(n1 > n2){
+        printf("O maior é o %d \n", n1);
+    }else {
+        if(n1 < n2){
+            printf("O maior é o %d \n", n2);
+        }else{
+           printf("%d é igual a %d \n", n1, n2);
+        }
+    }
 }
