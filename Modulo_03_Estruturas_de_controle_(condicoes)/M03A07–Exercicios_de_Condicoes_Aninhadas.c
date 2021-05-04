b9f63060cba50444981434e8b766b107b593f9cf
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-05-04 19:50:59 +0100

    M03A07 – Exercícios de Condições Aninhadas

--- Modulo_03_Estruturas_de_controle_(condicoes)/M03A07–Exercicios_de_Condicoes_Aninhadas.c
+++ Modulo_03_Estruturas_de_controle_(condicoes)/M03A07–Exercicios_de_Condicoes_Aninhadas.c
@@ -8,5 +8,15 @@
 */
 
 /*
- <<< EX024 -  >>>
+ <<< EX024 - Ordem em dois números >>>
+
+ Me diga dois números e eu colocarei
+ os dois em ordem crescente.
+
+ Primeiro número: 9
+
+ Segundo número: 4
+
+ Os números em ordem são 4 e 9.
+
 */
