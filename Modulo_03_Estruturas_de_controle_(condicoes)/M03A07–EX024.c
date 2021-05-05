4177a5ab9422e39931a5ff331588d8097c9ad289
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-05-05 18:00:27 +0100

    M03A07 –  EX024

--- Modulo_03_Estruturas_de_controle_(condicoes)/M03A07–EX024.c
+++ Modulo_03_Estruturas_de_controle_(condicoes)/M03A07–EX024.c
@@ -4,4 +4,19 @@
 void main(){
     setlocale(LC_ALL, "Portuguese");
 
+    printf("<<< EX024 - Ordem em dois números >>> \n\n");
 }
+
+/*
+ <<< EX024 - Ordem em dois números >>>
+
+ Me diga dois números e eu colocarei
+ os dois em ordem crescente.
+
+ Primeiro número: 9
+
+ Segundo número: 4
+
+ Os números em ordem são 4 e 9.
+
+*/
