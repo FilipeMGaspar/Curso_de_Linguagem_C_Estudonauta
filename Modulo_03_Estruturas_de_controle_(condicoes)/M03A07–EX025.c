941cd8197708a961f1f8694dcae04bf53857f241
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-05-05 18:56:56 +0100

    M03A07 – EX025

--- Modulo_03_Estruturas_de_controle_(condicoes)/M03A07–EX025.c
+++ Modulo_03_Estruturas_de_controle_(condicoes)/M03A07–EX025.c
@@ -1,39 +1,20 @@
 #include <stdio.h>
 #include <locale.h>
+#include <time.h>
 
 void main(){
     setlocale(LC_ALL,"Portuguese");
     printf("<<< EX025 - Serviço Militar V2.0 >>> \n\n");
+    time_t t;
+    time(&t);
+    struct tm *data;
+    data = localtime(&t);
 
-    printf("Atualmente estamios no ano de ");
+    int ano_atual = data->tm_year + 1900;
+
+    printf("Atualmente estamios no ano de %d \n", ano_atual);
 }
 
 /*
-<<< EX025 - Serviço Militar V2.0 >>>
 
-Atualmente estamos no ano de 2021.
-Em que ano você nasceu? 1978
-
-------------------------------------
-Sua idade atual é 43 anos.
-Seu alistamento foi em 1996. Já se +assaram 25 anos.
--------------------------------------------------
-
-
-
-Em que ano você nasceu? 2010
-
-------------------------------------
-Sua idade atual é 11 anos.
-Seu alistamento será em 2028. Ainda faltam 7 anos.
--------------------------------------------------
-
-
-
-Em que ano você nasceu? 2003
-
-------------------------------------
-Sua idade atual é 18 anos.
-Você completa 18 anos exectamente em 2021. Vá se alistar.
--------------------------------------------------
-/*
+*/
