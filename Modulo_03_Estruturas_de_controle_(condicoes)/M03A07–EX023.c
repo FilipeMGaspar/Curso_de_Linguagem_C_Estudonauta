7e98a64f9a1d9180ac0c0d8dddd00f8725eb31cb
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-05-05 17:49:52 +0100

    M03A07 –  EX023

--- Modulo_03_Estruturas_de_controle_(condicoes)/M03A07–EX023.c
+++ Modulo_03_Estruturas_de_controle_(condicoes)/M03A07–EX023.c
@@ -4,4 +4,16 @@
 void main(){
     setlocale(LC_ALL,"Portuguese");
 
+    printf("<<< EX023 - positivo ou Negativo >>> \n\n");
+    printf(" Me diga um número e eu te direi se \n");
+    printf("ele é POSITIVO, NEGATIVO ou NULO.");
 }
+
+/*
+ <<< EX023 - positivo ou Negativo >>>
+
+ Me diga um número e eu te direi se
+ ele é POSITIVO, NEGATIVO ou NULO.
+
+ Digite um número
+*/
