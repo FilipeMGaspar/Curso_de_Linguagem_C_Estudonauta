79eed5a76ea52c01c7177ff3d296e88fd533ccea
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-05-05 18:28:32 +0100

    M03A07 – EX025

--- Modulo_03_Estruturas_de_controle_(condicoes)/M03A07–EX025.c
+++ Modulo_03_Estruturas_de_controle_(condicoes)/M03A07–EX025.c
@@ -5,3 +5,33 @@ void main(){
     setlocale(LC_ALL,"Portuguese");
 
 }
+
+/*
+<<< EX025 - Serviço Militar V2.0 >>>
+
+Atualmente estamos no ano de 2021.
+Em que ano você nasceu? 1978
+
+------------------------------------
+Sua idade atual é 43 anos.
+Seu alistamento foi em 1996. Já se +assaram 25 anos.
+-------------------------------------------------
+
+
+
+Em que ano você nasceu? 2010
+
+------------------------------------
+Sua idade atual é 11 anos.
+Seu alistamento será em 2028. Ainda faltam 7 anos.
+-------------------------------------------------
+
+
+
+Em que ano você nasceu? 2003
+
+------------------------------------
+Sua idade atual é 18 anos.
+Você completa 18 anos exectamente em 2021. Vá se alistar.
+-------------------------------------------------
+/*
