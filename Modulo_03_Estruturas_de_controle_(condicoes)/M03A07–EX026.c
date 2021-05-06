e8d79160f7bd7f8880c560195b25bd692154246a
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-05-06 19:18:31 +0100

    M03A07 – EX026

--- Modulo_03_Estruturas_de_controle_(condicoes)/M03A07–EX026.c
+++ Modulo_03_Estruturas_de_controle_(condicoes)/M03A07–EX026.c
@@ -5,3 +5,36 @@ void main(){
     setlocale(LC_ALL,"Portuguese");
 
 }
+
+/*
+<<< EX026 -  Qual é o seu Estado? >>>
+Em que estado do Brasil Você nasceu? RJ
+
+Nascendo em RJ Você é CARIOCA
+
+
+Em que estado do Brasil Você nasceu? SP
+
+Nascendo em SP Você é PAULISTA
+
+
+Em que estado do Brasil Você nasceu? MG
+
+Nascendo em MG Você é MINEIRO
+
+
+Em que estado do Brasil Você nasceu? AL
+
+Nascendo em AL Você é ALAGOANO
+
+
+Em que estado do Brasil Você nasceu? RN
+
+Nascendo em RN Você é POTIGUAR
+
+
+Em que estado do Brasil Você nasceu? TO
+
+Nascendo em TO Você é natural da sua cidade, mas ainda não sei como te chamar!
+
+*/
