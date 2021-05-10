0166339db6824439ff16f365e899c9ac016330cd
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-05-10 12:33:41 +0100

    M03A09 – EX028

--- Modulo_03_Estruturas_de_controle_(condicoes)/M03A09–EX028.c
+++ Modulo_03_Estruturas_de_controle_(condicoes)/M03A09–EX028.c
@@ -3,5 +3,51 @@
 
 void main(){
     setlocale(LC_ALL,"Portuguese");
+    int num1, num2;
+
+    printf("<<< EX028 - Super Calculadora v1.0 >>> \n\n");
+    printf("VALOR 1 = ");
+    fflush(stdin);
+    scanf("%d", num1);
 
 }
+
+/*
+<<< EX028 - Super Calculadora v1.0 >>>
+
+VALOR 1 = 5
+VALOR 2 = 8
+
+===========================
++   ADIÇÃO
+-   SUBTRAÇÃO
+*   MULTIPLICAÇÂO
+/   DIVISÃO
+============================
+Digite a sua opção => +
+
+-------------------------------------
+O resultado de 5 + 8 é igual a 13.
+-------------------------------------
+
+VOLTE SEMPRE!
+
+<<< EX028 - Super Calculadora v1.0 >>>
+
+VALOR 1 = 5
+VALOR 2 = 8
+
+===========================
++   ADIÇÃO
+-   SUBTRAÇÃO
+*   MULTIPLICAÇÂO
+/   DIVISÃO
+============================
+Digite a sua opção => )
+
+-------------------------------------
+Não foi posssível fazer a operação. Tente Novamente.
+-------------------------------------
+
+VOLTE SEMPRE!
+*/
