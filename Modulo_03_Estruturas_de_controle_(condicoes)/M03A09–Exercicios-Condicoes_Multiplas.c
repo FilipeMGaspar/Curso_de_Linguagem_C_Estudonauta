39a8db5e7db6a0944604ae19b5a89279e814aeb8
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-05-10 11:57:36 +0100

    M03A09 – Exercícios Condições Múltiplas

--- Modulo_03_Estruturas_de_controle_(condicoes)/M03A09–Exercicios-Condicoes_Multiplas.c
+++ Modulo_03_Estruturas_de_controle_(condicoes)/M03A09–Exercicios-Condicoes_Multiplas.c
@@ -18,4 +18,22 @@ O resultado de 5 + 8 é igual a 13.
 
 VOLTE SEMPRE!
 
+<<< EX028 - Super Calculadora v1.0 >>>
+
+VALOR 1 = 5
+VALOR 2 = 8
+
+===========================
++   ADIÇÃO
+-   SUBTRAÇÃO
+*   MULTIPLICAÇÂO
+/   DIVISÃO
+============================
+Digite a sua opção => )
+
+-------------------------------------
+Não foi posssível fazer a operação. Tente Novamente.
+-------------------------------------
+
+VOLTE SEMPRE!
 */
