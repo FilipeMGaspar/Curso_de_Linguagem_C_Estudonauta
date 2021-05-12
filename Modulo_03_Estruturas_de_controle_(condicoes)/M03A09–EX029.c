72161ad788cae78f79f86ede73e9120e50ce783e
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-05-12 09:43:42 +0100

    M03A07 – EX029

--- Modulo_03_Estruturas_de_controle_(condicoes)/M03A09–EX029.c
+++ Modulo_03_Estruturas_de_controle_(condicoes)/M03A09–EX029.c
@@ -5,3 +5,49 @@ void main(){
     setlocale(LC_ALL,"Portuguese");
 
 }
+
+/*
+<<< EX029 - Seu peso nos planetas v1.0 >>>
+
+    Seu peso na Terra(Kg) = 91
+
+
+        ESCOLHA UM PLANETA
+        ===================
+        1   Mercúrio
+        2   Vênus
+        3   Marte
+        4   Júpiter
+        5   Saturno
+        6   Urano
+        ===================
+        Digite a sua opção => 4
+
+        ----------------------------------
+        No planeta JÚPITER, seu peso seria 240.24 Kg
+        --------------------------------------
+        VOLTE SEMPRE.
+
+
+<<< EX029 - Seu peso nos planetas v1.0 >>>
+
+    Seu peso na Terra(Kg) = 91
+
+
+        ESCOLHA UM PLANETA
+        ===================
+        1   Mercúrio
+        2   Vênus
+        3   Marte
+        4   Júpiter
+        5   Saturno
+        6   Urano
+        ===================
+        Digite a sua opção => 9
+
+        ----------------------------------
+        Seu peso não pode ser calculado para outros planetas.
+        Tente Novamente.
+        --------------------------------------
+        VOLTE SEMPRE.
+*/
