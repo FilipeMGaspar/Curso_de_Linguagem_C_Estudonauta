ee0b2bce3a9556b60470149a6c91a0023c97844f
Filipe Miguel Martins Gaspar <fipewmc2@gmail.com>
2021-05-13 12:06:18 +0100

    M03A07 – EX030

--- Modulo_03_Estruturas_de_controle_(condicoes)/M03A09–EX030.c
+++ Modulo_03_Estruturas_de_controle_(condicoes)/M03A09–EX030.c
@@ -3,5 +3,50 @@
 
 void main(){
     setlocale(LC_ALL,"Portuguese");
+    float preco;
+
+    printf("<<< EX030 - Preço por Época >>> \n\n");
 
 }
+
+/*
+<<< EX030 - Preço por Época >>>
+
+Digite o preço de um produto R$ 500
+
+    ESCOLHA UMPERÍODO
+    ===================
+    1   Carnaval [+10%]
+    2   Férias Escolares [+20%]
+    3   Dia das Crianças [+5%]
+    4   Black Fryday [-30%]
+    5   Natal [-5%]
+    =====================
+    Digite a sua opção => 2
+
+    -----------------------------------------
+    Na época de Férias Escolares, o preço do produdo vai para R$ 600,00.
+    ------------------------------------------------
+    VOLTE SEMPRE!
+
+
+
+    <<< EX030 - Preço por Época >>>
+
+Digite o preço de um produto R$ 500
+
+    ESCOLHA UMPERÍODO
+    ===================
+    1   Carnaval [+10%]
+    2   Férias Escolares [+20%]
+    3   Dia das Crianças [+5%]
+    4   Black Fryday [-30%]
+    5   Natal [-5%]
+    =====================
+    Digite a sua opção => 9
+
+    -----------------------------------------
+    Em épocas assim, Mantenha o preço do produdo em R$ 500,00.
+    ------------------------------------------------
+    VOLTE SEMPRE!
+*/
