c35d716c188ac5e23d565983263cf82dd0fb09cd
filipemgaspar <fipewmc2@gmail.com>
2021-06-05 09:25:43 +0100

    Exercício 01 - Frase na Tela

---
+++ Exercicios_Assimilacao/Exercicio_02_Etiqueta.c
@@ -0,0 +1,10 @@
+/*
+2. Etiqueta -
+Elabore um programa que pede:
+ seu nome, endereço, CEP e telefone.
+
+Ele deve imprirmir:
+ seu nome completo na primeira linha,
+ seu endereço na segunda,
+ e o CEP e telefone na terceira.
+*/
