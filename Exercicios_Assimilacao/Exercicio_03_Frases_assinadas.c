9281064b95fcbfe037c911f2548255942cc99c8d
filipemgaspar <fipewmc2@gmail.com>
2021-06-05 09:43:57 +0100

    Exercício 03 - Frases Assinadas

---
+++ Exercicios_Assimilacao/Exercicio_03_Frases_assinadas.c
@@ -0,0 +1,15 @@
+/*
+3. Frases assassinas -
+ Faça um programa que mostre na tela algumas frases assassinas,
+ que são aquelas que fazem com muitas idéias sejam perdidas
+ antes que amadureçam ou seja aprofundadas.
+ Eis alguns exemplos (elabore também os seus):
+
+"Isto não vai dar certo"
+"Você nunca vai conseguir"
+"Você vai se arrepender"
+"Não vai dar em nada"
+"Está tudo errado!"
+"Programar é muito dificil!"
+"Sonha pouco, mas o melhor é não sonhar!"
+*/
