cff731cb6dc1ac8fb12cf2f73e23ea69ed8276af
filipemgaspar <fipewmc2@gmail.com>
2021-06-07 23:32:36 +0100

    Exercicio 06 - Quadrado na tela

---
+++ Exercicios_Assimilacao/Printf/Exercicio_06_quadrado_na_tela.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void main(){
+    printf("\txxxxx \n");
+    printf("\tx   x \n");
+    printf("\tx   x \n");
+    printf("\tx   x \n");
+    printf("\txxxxx \n");
+
+}
+
+/*
+6. Quadrado -
+Escrever um programa que mostre a seguinte figura no alto da tela:
+
+XXXXX
+X   X
+X   X
+X   X
+XXXXX
+*/
