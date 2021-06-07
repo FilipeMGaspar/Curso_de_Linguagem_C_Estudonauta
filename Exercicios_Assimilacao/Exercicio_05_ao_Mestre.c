e57e910510723f6fc38d1c5f36bc324774f083dc
filipemgaspar <fipewmc2@gmail.com>
2021-06-07 23:17:59 +0100

    Exercicio 05 - Bilhete ao Professor

---
+++ Exercicios_Assimilacao/Exercicio_05_ao_Mestre.c
@@ -0,0 +1,7 @@
+/*
+5. Ao mestre -
+Escreva um bilhete ao seu professor,
+informando seus objetivos nesta disciplina
+e o que espera dela e do professor.
+Implemente um programa que mostra seu bilhete na tela.
+*/
