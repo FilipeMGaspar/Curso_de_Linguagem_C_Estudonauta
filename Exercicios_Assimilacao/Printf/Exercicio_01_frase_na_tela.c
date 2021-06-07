18a62292eec251792b6baac3503a581d86ae567e
filipemgaspar <fipewmc2@gmail.com>
2021-06-07 23:31:11 +0100

    Exercício 01 - Frase na Tela

---
+++ Exercicios_Assimilacao/Printf/Exercicio_01_frase_na_tela.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <locale.h>
+
+void main(){
+ setlocale(LC_ALL,"Portuguese");
+
+ printf("\"O primeiro programa \n a gente nunca esquece!\". \n");
+}
+
+/*
+1. Frase na tela  -
+Implemente um programa
+que escreve na tela a frase
+
+"O primeiro programa
+a gente nunca esquece!".
+*/
