bd67fc3a0239b31459b399caa9d24f00da775d5b
filipemgaspar <fipewmc2@gmail.com>
2021-06-10 11:36:35 +0100

    Exercício 7. Tabela de notas

---
+++ Exercicios_Assimilacao/Printf/Exercicio_07_Tabela_de_notas.c
@@ -0,0 +1,11 @@
+/*
+7. Tabela de notas
+Escreva um programa que produza a seguinte saída na tela:
+
+ALUNO(A)          NOTA
+=========         =====
+ALINE              9.0
+MÁRIO              DEZ
+SÉRGIO             4.5
+SHIRLEY            7.0
+*/
