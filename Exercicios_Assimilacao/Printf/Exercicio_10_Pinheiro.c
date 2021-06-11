898e9eb05f0b8b658708c268983138367aefef8a
filipemgaspar <fipewmc2@gmail.com>
2021-06-11 08:17:18 +0100

    Exercício 10 - Pinheiro na tela

---
+++ Exercicios_Assimilacao/Printf/Exercicio_10_Pinheiro.c
@@ -0,0 +1,17 @@
+/*
+10. Pinheiro  -
+Implemente um programa que desenhe um "pinheiro" na tela, similar ao abaixo.
+Enriqueça o desenho com outros caracteres, simulando enfeites.
+
+       X
+      XXX
+     XXXXX
+    XXXXXXX
+   XXXXXXXXX
+  XXXXXXXXXXX
+ XXXXXXXXXXXXX
+XXXXXXXXXXXXXXX
+       XX
+       XX
+      XXXX
+*/
