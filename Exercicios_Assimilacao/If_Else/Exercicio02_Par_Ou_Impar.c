db558b7f3609ced3c49ae9f0655a5048767839d3
filipemgaspar <fipewmc2@gmail.com>
2021-06-13 16:01:07 +0100

    Exercicio 02 - Par ou Ímpar

---
+++ Exercicios_Assimilacao/If_Else/Exercicio02_Par_Ou_Impar.c
@@ -0,0 +1,5 @@
+/*
+Exercicio 02 - Par ou Ímpar
+Escreva um programa que recebe um inteiro e diga se é par ou ímpar.
+Use o operador matemático % (resto da divisão ou módulo) e o teste condicional if.
+*/
