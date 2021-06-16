5a9ff4d1afe7edcdb678ac1b4fa5a600edbbbb2a
filipemgaspar <fipewmc2@gmail.com>
2021-06-16 21:52:11 +0100

    Exercicio 04 - Idade do Doardor de Sangue

---
+++ Exercicios_Assimilacao/If_Else/Exercicio04_Idade_Para_Dar_Sange.c
@@ -0,0 +1,8 @@
+/*
+4. Salve vidas dê sangue
+Para doar sangue é necessário ter entre 18 e 67 anos.
+Faça um aplicativo na linguagem C
+Que pergunte a idade de uma pessoa
+diga se ela pode doar sangue ou não.
+Use alguns dos operadores lógicos OU (||) e E (&&).
+*/
