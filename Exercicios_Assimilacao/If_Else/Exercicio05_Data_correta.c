fe4e4105e7d5039948a607f26d4ad2c248cd74c9
filipemgaspar <fipewmc2@gmail.com>
2021-06-17 12:41:59 +0100

    Exercício 05 - Verificação da Data

--- Exercicios_Assimilacao/If_Else/Exercicio05_Data_correta.c
+++ Exercicios_Assimilacao/If_Else/Exercicio05_Data_correta.c
@@ -5,14 +5,16 @@
 void main(){
     setlocale(LC_ALL,"Portuguese");
 
+    //Captura da data do sistema
     time_t t;
     time(&t);
     struct tm *data;// tm nome do struct data ponteiro para tm
     data = localtime(&t);
-
     int dia = data->tm_mday;
     int mes = data->tm_mon +1;;
     int ano = data->tm_year + 1900;
+
+
     printf("%d - %d - %d \n",dia, mes, ano);
 }
 
