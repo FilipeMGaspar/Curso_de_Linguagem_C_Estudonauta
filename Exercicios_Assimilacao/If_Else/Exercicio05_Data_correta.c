3a730c9d3337e6f1cb5147e8457dcb5ef62b55cb
filipemgaspar <fipewmc2@gmail.com>
2021-06-17 12:29:16 +0100

    Exercício 05 - Verificação da Data

---
+++ Exercicios_Assimilacao/If_Else/Exercicio05_Data_correta.c
@@ -0,0 +1,8 @@
+/*
+5. Data Correcta
+Escreva um programa
+Pergunte o dia, mês e ano do aniversário de uma pessoa
+diga se a data é válida ou não.
+Caso não seja, diga o motivo.
+Suponha que todos os meses tem 31 dias e que estejamos no ano de 2013.
+*/
