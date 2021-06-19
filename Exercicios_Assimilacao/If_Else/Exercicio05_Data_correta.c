bed7c810ffb802bec0f55c46bbfae995c8f2695c
filipemgaspar <fipewmc2@gmail.com>
2021-06-19 08:08:14 +0100

    Exercício 05 - Verificação da Data

--- Exercicios_Assimilacao/If_Else/Exercicio05_Data_correta.c
+++ Exercicios_Assimilacao/If_Else/Exercicio05_Data_correta.c
@@ -27,17 +27,17 @@ void main(){
     scanf("%d",&anoAniv);
 
     if(anoAniv < (ano - 120)){
-        printf("\n [ ERRO 365] Ano Inválido! \n");
+        printf("\n[ ERRO 365] Ano Inválido! \n");
         printf("É Muito pouco provavel que você tenha %d anos\n",(ano - anoAniv));
         printf("Por favor indique um ano entre %d e %d \n", (ano -120), ano);
     }else if(anoAniv > ano){
-        printf("\n [ ERRO 365] Ano Inválido! \n");
+        printf("\n[ ERRO 365] Ano Inválido! \n");
         printf("Para o ano de %d ainda faltam %d anos \n", anoAniv, (anoAniv - ano));
         printf("Por favor indique um ano entre %d e %d \n", (ano -120), ano);
     }
 
     if(diaAniv < 1 || diaAniv > 31){
-        printf("\n [ ERRO 12] Dia Inexistente! \n");
+        printf("\n[ ERRO 12] Dia Inexistente! \n");
         printf("\nO dia \" %d \"", diaAniv );
         printf(" não é válido!\n O mês inicia sempre a dia 1 e tem no máximo 31 dias! \n");
 
