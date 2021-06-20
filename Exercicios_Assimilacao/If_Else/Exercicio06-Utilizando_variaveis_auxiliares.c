be624a7be9bab044f1fd6bf378ca2d8a3fbe6755
filipemgaspar <fipewmc2@gmail.com>
2021-06-20 17:32:20 +0100

    Exercício 06 Utilizando Variaveis Auxiliares

---
+++ Exercicios_Assimilacao/If_Else/Exercicio06-Utilizando_variaveis_auxiliares.c
@@ -0,0 +1,8 @@
+/*
+Exercico 06
+Peça um número ao usuário e armazene ele na variável x.
+Depois peça outro número e armazene na variável y.
+Mostre esses números.
+Em seguida, faça com que x passe a ter o valor de y,
+e que y passe a ter o valor de x.
+*/
