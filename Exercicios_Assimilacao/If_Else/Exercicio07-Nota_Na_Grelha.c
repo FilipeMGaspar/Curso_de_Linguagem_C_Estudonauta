7b111caa94f35e2fdb49e70870b3d74864815b16
filipemgaspar <fipewmc2@gmail.com>
2021-06-24 12:24:18 +0100

    Exercício 07 - Avaliação de Alunos

---
+++ Exercicios_Assimilacao/If_Else/Exercicio07-Nota_Na_Grelha.c
@@ -0,0 +1,13 @@
+/*
+Desafio 2. Crie um programa
+Que recebe uma nota (entre 0.0 e 10.0)
+Verifique se:
+ passou direto,
+ ficou de recuperação
+ foi reprovado na matéria.
+
+A regra é a seguinte:
+Nota 7 ou mais: passou direto
+Entre 4 e 7: tem direito de fazer uma prova de recuperação
+Abaixo de 4: reprovado direto
+*/
