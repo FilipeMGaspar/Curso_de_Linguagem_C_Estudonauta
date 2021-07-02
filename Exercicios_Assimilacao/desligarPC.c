49c0c82c1035a5ae6c2b89050b775ef650651de0
filipemgaspar <fipewmc2@gmail.com>
2021-07-02 21:44:12 +0100

    Desligar PC

--- Exercicios_Assimilacao/desligarPC.c
+++ Exercicios_Assimilacao/desligarPC.c
@@ -10,6 +10,28 @@ void main(){
 
     int diaDaSemana = data->tm_wday + 1;
 
-
-    printf("%d", diaDaSemana);
+    switch (diaDaSemana){
+        case 1:
+            printf("Domingo");
+            break;
+        case 2:
+            printf("Segunda");
+            break;
+        case 3:
+            printf("Terça");
+            break;
+        case 4:
+            printf("Quarta");
+            break;
+        case 5:
+            printf("Quinta");
+            break;
+        case 6:
+            printf("Sexta");
+            break;
+        case 7:
+            printf("Sabado");
+            break;
+    }
+   // printf("%d", diaDaSemana);
 }
